bf37cf755afcd7740f5a1f79039cd2264c27e8b7
alisaid-sm <alisaid7170@gmail.com>
2025-01-25 23:46:49 +0700

    feat: bubble venue

---
+++ Assets/Scripts/Package.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Package : MonoBehaviour
+{
+    public Transform bubble;
+    public float bubbleMaxScale;
+
+    public void SetBubbleType()
+    {
+        Debug.Log("SetBubbleType");
+    }
+}
