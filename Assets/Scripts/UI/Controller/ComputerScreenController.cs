a3ccee582d41cd8d920bc3313166773fcd2c0f0d
Anang Syaifur <anangsyfr123@gmail.com>
2025-01-26 09:21:58 +0700

    feat: point 2 done

--- Assets/Scripts/UI/Controller/ComputerScreenController.cs
+++ Assets/Scripts/UI/Controller/ComputerScreenController.cs
@@ -10,9 +10,12 @@ public class ComputerScreenController : MonoBehaviour
     private GameObject itemLine;
     [SerializeField]
     private GameObject spawnPoint;
+
+    private GameObject[] packages;
     void Start()
     {
         gameUIManager = GameObject.FindWithTag("GameUIManager").GetComponent<GameUIManager>();
+
         RenderTasks();
     }
 
@@ -24,16 +27,23 @@ public class ComputerScreenController : MonoBehaviour
         }
 
         List<ITask> tasks = gameUIManager._itemTasks;
+        Debug.Log(tasks[0].package);
         for (int i = 0; i < tasks.Count; i++)
         {
             int taskIndex = i;
             LineController line = Instantiate(itemLine, this.transform).GetComponent<LineController>();
+            Debug.Log(tasks[0].package.packageName);
             line.SetContent(tasks[i].client.clientName, tasks[i].package.packageName, tasks[i].status, () => SetOnProgress(taskIndex));
         }
     }
 
     void SetOnProgress(int index)
     {
+        packages = GameObject.FindGameObjectsWithTag("Package");
+        foreach (GameObject obj in packages)
+        {
+            Destroy(obj);
+        }
         foreach (Transform child in spawnPoint.transform)
         {
             Destroy(child.gameObject);
